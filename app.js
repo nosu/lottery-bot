43ba42dd28f6ebfa82abf6cc6c6454ada30b849d
Wataru Inoue <i@nosu.biz>
2016-06-19 23:49:50 +0900

    Finish implementing the basic functions

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ app.use(bodyParser.json());
 const SESSION_EXPIRE = 20000; // 20 seconds
 const BASE_URI = "https://trialbot-api.line.me";
 const SEND_URI = BASE_URI + '/v1/events';
+
+/**
+ * "session" stores who sent the message and the list of the person names.
+ * Each session will be deleted automatically SESSION_EXPIRE mil seconds later.
+ * Example: { ubcf6832fae5a186d9b8ac7261e3ff000: ['John', 'Doe'] }
+ */
 let session = {};
 
 app.post('/callback', (req, res) => {
@@ -16,56 +22,90 @@ app.post('/callback', (req, res) => {
   body.result.forEach((msg) => {
     if(!session[msg.content.from]) {
       // if the session doesn't exist
-      const content = msg.content;
-      const persons = content.split('\n');
-      console.log('content: ', content);
+      console.log('content: ', msg.content);
+      const persons = msg.content.text.split('\n').map(p => p.trim());
       if(persons.length >= 2) {
-        addSessionTimer(msg.content.from, SESSION_EXPIRE);
-        const newMsg = persons.reduce((prev, cur) => { return (prev + cur + "さん, "); }, "") + "から何人選びますか？";
-        sendMessage({
-          to: msg.content.from,
-          toChannel: 1383378250, // Fixed value
-          eventType: "138311608800106203", // Fixed value
-          content: newMsg,
-        });
+        addSessionTimer(msg.content.from, persons, SESSION_EXPIRE);
+        const newMsg = `${persons.join("さん, ")}さんから何人選ぶ？数字で教えてー`;
+        console.log('sendMessage');
+        sendMessage(newMsg, [msg.content.from]);
       } else {
         const newMsg = "1人だけじゃ抽選できないよ！";
-        sendMessage({
-          to: msg.content.from,
-          toChannel: 1383378250, // Fixed value
-          eventType: "138311608800106203", // Fixed value
-          content: newMsg,
-        });
+        sendMessage(newMsg, [msg.content.from]);
       }
-      res.send('OK');
     } else {
       // if the session exists
-      res.send('OK');
+      console.log('session: ', session);
+      const matchResult = msg.content.text.match(/([0-9]+)/);
+      const persons = session[msg.content.from];
+      if(matchResult) {
+        const numberOfWinners = parseInt(matchResult[0]);
+        const winners = chooseRandomItems(persons, numberOfWinners);
+        const newMsg = `${winners.join("さん, ")}さんが当たりだよ！`;
+        sendMessage(newMsg, [msg.content.from]);
+      } else {
+        const winners = chooseRandomItems(persons, 1);
+        const newMsg = `よくわからないから1人だけ選んだ結果…n${winnes[0]}さんが当たりだよ！`;
+        sendMessage(newMsg, [msg.content.from]);
+      }
+      deleteSession(msg.content.from);
     }
   });
+  res.send('OK');
 });
 
-const addSessionTimer = (id, expire) => {
-  session[id] = true;
-  setTimeout(() => { delete session[id]; }, expire);
+const addSessionTimer = (id, persons, expire) => {
+  session[id] = persons;
+  setTimeout(deleteSession.bind(null, id), expire);
 };
 
-const sendMessage = (content) => {
+const deleteSession = (id) => {
+  console.log(`The session <${id}> is deleted.`);
+  delete session[id];
+};
+
+const chooseRandomItems = (array, number) => {
+  let a = array.concat();
+  let r = [];
+  let l = array.length;
+  let n = Math.min(number, array.length);
+  while(n-- > 0) {
+    let i = Math.floor(Math.random() * l--);
+    r.push(a[i]);
+    a.splice(i, 1);
+  }
+  return r;
+}
+
+const sendMessage = (text, toId) => {
   return new Promise((resolve, reject) => {
-    request({
+    const options = {
       uri: SEND_URI,
       method: "POST",
+      json: true,
       headers: {
         "Content-Type": "application/json; charset=UTF-8",
         "X-Line-ChannelID": process.env.LINE_CHANNEL_ID,
         "X-Line-ChannelSecret": process.env.LINE_CHANNEL_SECRET,
         "X-Line-Trusted-User-With-ACL": process.env.LINE_CHANNEL_MID,
       },
-      body: content,
-    }, (err, res, body) => {
+      body: {
+        to: toId,
+        toChannel: 1383378250, // Fixed value
+        eventType: "138311608800106203", // Fixed value
+        content: {
+          contentType: 1,
+          toType: 1,
+          text: text,
+        }
+      }
+    };
+    console.log('options: ', options);
+    request(options, (err, res, body) => {
       if(err) {
         reject(new Error(err));
       }
+      console.log('request complete without error\nbody: ', body);
       resolve(body);
     });
   });
